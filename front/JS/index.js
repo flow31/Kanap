f8c619bae43ba74802e06f0d292bc715c02f26eb
flow31 <florian.coquene@gmail.com>
2023-01-04 10:19:55 +0100

    add comments

--- front/JS/index.js
+++ front/JS/index.js
@@ -1,9 +1,12 @@
-// Fonction async await fetch pour récupérer les datas sur l'api
 async function getDatas() {
+  // Envoie une requête HTTP GET à l'API et attend la réponse
   const res = await fetch('http://localhost:3000/api/products');
+  // Convertit la réponse en format JSON et la stock dans la variable `datas`
   const datas = await res.json();
-  // Boucle for ajout html
+
+  // Pour chaque élément du tableau `datas`
   for (let data of datas) {
+    // Crée une chaîne de caractères qui représente un élément HTML contenant des informations sur un produit
     const product = `
     <a href="./product.html?id=${data._id}">
             <article>
@@ -13,10 +16,13 @@ async function getDatas() {
             </article>
           </a>
     `;
-    // Selection de l'élément HTML
+
+    // Sélectionne l'élément HTML qui a l'ID "items"
     const section = document.querySelector('#items');
+    // Insère l'élément HTML généré à la fin de la section sélectionnée
     section.insertAdjacentHTML('beforeend', product);
   }
 }
-// Lancement de la fonction
+
+// Appelle la fonction
 getDatas();
--- front/JS/product.js
+++ front/JS/product.js
@@ -1,66 +1,73 @@
-//Création const pour rechercher l'id dans l'url
+// Récupération des paramètres de l'URL
 const urlSearch = window.location.search;
 const urlParams = new URLSearchParams(urlSearch);
 const productId = urlParams.get('id');
-// Fonction async await + recherche de l'id
+
+// Récupération des données du produit depuis l'API
 async function getDatas() {
   const res = await fetch('http://localhost:3000/api/products/' + productId);
   const data = await res.json();
-  // Selection sur le code html
+
+  // Mise à jour du titre de la page
   document.querySelector('title').innerHTML = data.name;
+
+  // Mise à jour de l'image du produit
   document.querySelector(
     '.item__img'
   ).innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+
+  // Mise à jour du nom, du prix et de la description du produit
   document.querySelector('#title').innerHTML = data.name;
   document.querySelector('#price').innerHTML = data.price;
   document.querySelector('#description').innerHTML = data.description;
 
-  // Boucle for of pour les options
-  //Création boucle pour choix des couleurs
+  // Mise à jour des options de couleur du produit
   for (let option of data.colors) {
     const color = `<option value="${option}">${option}</option>`;
 
     document.querySelector('#colors').insertAdjacentHTML('beforeend', color);
   }
 }
-//Lancement de la fonction
+
+// Récupération des données du produit depuis l'API
 getDatas();
-// Ecoute au clic + selection html
-// Création fonction pour le localstorage
+
+// Gestion de l'ajout au panier
 document.querySelector('#addToCart').addEventListener('click', () => {
+  // Récupération de la couleur et de la quantité sélectionnées par l'utilisateur
   const color = document.querySelector('#colors').value;
   const quantity = document.querySelector('#quantity').value;
-  // Si pas de couleurs sélectionner alors message d'alerte
+
+  // Validation de la couleur et de la quantité
+  // Si la couleur est vide
   if (color == '') {
     alert('Choisissez une couleur');
     return;
   }
-  // Si quantité en dessous de 0 ou au dessus de 100 alors message d'alerte
+  // Si la quantité n'est pas valide
   if (quantity < 1 || quantity > 100) {
     alert('Mettre une quantité valide');
     return;
   }
 
+  // Lancement de la fonction addToCart
   addToCart();
 });
-//Fonction ajout panier
+
+// Fonction d'ajout au panier
 function addToCart() {
-  // Si le localstorage est rempli alors création d'un tableau cart
+  // Récupération du panier dans le local storage
   const cart =
     localStorage.getItem('cart') != null ||
     localStorage.getItem('cart') != undefined
       ? JSON.parse(localStorage.getItem('cart'))
       : [];
 
-  // récupération des éléments suivant : color, quantity, name, image
-  // Création de produit en réutilisant color, quantity, name, image et l'id du produit
-  //Selection des elements html
+  // Récupération des données du produit
   const color = document.querySelector('#colors').value;
-  //Proriété parseInt pour que les quantités soit des nombres entiers
   const quantity = parseInt(document.querySelector('#quantity').value);
   const name = document.querySelector('#title').innerHTML;
   const image = document.querySelector('.item__img').innerHTML;
-  //Création d'un objet pour  récupérer toutes les infos
   const product = {
     id: productId,
     color,
@@ -68,33 +75,36 @@ function addToCart() {
     name,
     quantity,
   };
-  // Condition si le tableau cart est vide alors ajout
+
+  // Si le panier est vide, ajout du produit
   if (cart.length <= 0) {
     cart.push(product);
     localStorage.setItem('cart', JSON.stringify(cart));
     alert('Produit ajouté au panier');
     return;
   }
-  // Comparaison entre le tableau et l'id récuperer sur l'api et la couleur
+
+  // Si le produit existe déjà dans le panier, mise à jour de la quantité
   const index = cart.findIndex(
     (product) => product.id === productId && product.color === color
   );
-  //Mise a jour quantité
   if (index != -1) {
-    // MEttre à jour le produit
     const newQuantity = quantity + cart[index].quantity;
 
+    // Validation de la quantité
     if (newQuantity > 100) {
       alert('Trop de produits');
       return;
     }
 
+    // Mise à jour de la quantité
     cart[index].quantity += quantity;
     localStorage.setItem('cart', JSON.stringify(cart));
     alert('La quantité du produit a été mise à jour');
     return;
   }
 
+  // Ajout du produit au panier
   cart.push(product);
   localStorage.setItem('cart', JSON.stringify(cart));
   alert('Produit ajouté au panier');
